b93e0f2a252b32af36a753d703dcdf4c1f4aca77
Lordishack1 <isaac.stertzbach@gmail.com>
2024-01-16 11:21:21 -0500

    got rid of primeng

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { TimelineModule } from 'primeng/timeline';
+
+
 
 @NgModule({
     exports: [
         MatButtonModule,
-        TimelineModule,
     ]
 })
 export class AngularMaterialModule { }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
+
+
+
 export class ExperienceComponent {
 
 }
